53a0f87d2e0f7724f590dda1638874c4adf1ef96
김채연 <@DESKTOP-QAROF8U>
2021-12-23 01:32:35 +0900

    다이어리 - 날짜별 카테고리 생성 생각중

--- cy/src/main/java/dao/mypage/DiaryDao.java
+++ cy/src/main/java/dao/mypage/DiaryDao.java
@@ -7,5 +7,11 @@ public class DiaryDao extends DB {
 	public DiaryDao() {
 		super();
 	}
+	
+	// 1. 다이어리 게시판 내에서 서브 게시판 생성하는 메소드
+	
+	
+	
+	
 
 }
--- cy/src/main/java/dto/Diary.java
+++ cy/src/main/java/dto/Diary.java
@@ -3,16 +3,12 @@ package dto;
 public class Diary {
 
 	private int diary_no;
-	private int mypage_no;
-	private String diary_date;
-	private String diary_content;
+	private int user_no;
 
-	public Diary(int diary_no, int mypage_no, String diary_date, String diary_content) {
+	public Diary(int diary_no, int user_no) {
 		super();
 		this.diary_no = diary_no;
-		this.mypage_no = mypage_no;
-		this.diary_date = diary_date;
-		this.diary_content = diary_content;
+		this.user_no = user_no;
 	}
 
 	public int getDiary_no() {
@@ -23,28 +19,12 @@ public class Diary {
 		this.diary_no = diary_no;
 	}
 
-	public int getMypage_no() {
-		return mypage_no;
+	public int getUser_no() {
+		return user_no;
 	}
 
-	public void setMypage_no(int mypage_no) {
-		this.mypage_no = mypage_no;
-	}
-
-	public String getDiary_date() {
-		return diary_date;
-	}
-
-	public void setDiary_date(String diary_date) {
-		this.diary_date = diary_date;
-	}
-
-	public String getDiary_content() {
-		return diary_content;
-	}
-
-	public void setDiary_content(String diary_content) {
-		this.diary_content = diary_content;
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
 	}
 
 }
